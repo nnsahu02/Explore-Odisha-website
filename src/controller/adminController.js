3f758c04eeb43f81234362e2dac718622fcb1697
Jyotibissoyi <jyotibissoyikhusbu2571@gmail.com>
2023-01-28 23:17:18 +0530

    added model

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -1,1 +1,20 @@
-const placeModel = require('../model/placeModel')
+const locationModel = require('../Models/locationModel');
+
+
+
+//>----------------------------CREATE-PRODUCT-API----------------------------<
+
+
+const createLocation = async function (req, res) {
+    try {
+      let data = req.body;
+      
+      const productsData = await locationModel.create(data);
+      return res.status(201).send({ status: true, data: productsData });
+    } catch (err) {
+                                  }
+  };
+
+
+
+module.exports.createLocation = createLocation
--- src/model/placeModel.js
+++ src/model/placeModel.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose')
 
-const placeModel = new mongoose.Schema({
-    
+const LocationModel = new mongoose.Schema({
+    place: {
+        type: String,
+      require:[ true, 'PLEASE PROVIDE placename'],
+        Unique: true  
+    },
+    image: {
+        type: String,
+        required:[true, 'PLEASE PROVIDE IMAGE']
+    },
+    description: {
+        type: String,
+        require:[ true, 'PLEASE PROVIDE Description']
+    },
+    category: {
+        type: String,
+        require:[ true, 'PLEASE PROVIDE category']
+    },
+    visitingTime: {
+        type: Number,
+        require:[ true, 'PLEASE PROVIDE  visitingTime  ']
+    },
+    dist: {
+        type: String,
+        require:[ true, 'PLEASE PROVIDE dist  ']
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+},
+    { timestamps: true })
 
-
-
-})
-
-module.exports = mongoose.model('Place', placeModel)
+module.exports = mongoose.model('Location', LocationModel)
