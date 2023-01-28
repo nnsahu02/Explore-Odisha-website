f6c0de20e29c667dc3263553847a21342254c5fa
nnsahu02 <nnsahu2022@gmail.com>
2023-01-29 00:30:00 +0530

    done backend

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,26 +1,19 @@
-const locationModel = require('../Model/locationModel'); 
+const locationModel = require('../model/locationModel');
 
-
-
-
-
+//>----------------------------fIND-Location-API----------------------------<
 const findLocation = async function (req, res) {
     try {
-      let queryParams = req.query
+        let queryParams = req.query
+        let data = await locationModel.find({ $and: [queryParams, { isDeleted: false }] })
 
-      let data = await locationModel.find({ $and: [queryParams, { isDeleted: false }] })
-  
-      if (data.length == 0) 
-         { return res.status(404).send({ status: false, message: "No data Found" })}
-      return res.status(200).send({ status: true, message: 'Location list', data: data })
+        if (data.length == 0) {
+            return res.status(404).send({ status: false, message: "No data Found" })
+        }
+        return res.status(200).send({ status: true, message: 'Location list', data: data })
     }
     catch (error) {
-      return res.status(500).send({ status: false, messasge: error.message })
+        return res.status(500).send({ status: false, messasge: error.message })
     }
-  }
+}
 
-
-
-
-
-  module.exports.findLocation = findLocation
+module.exports = { findLocation }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,19 @@
 const exprss = require('express')
 const router = exprss.Router()
-const locationController= require('../controller/userController')
-
+const { findLocation } = require('../controller/userController')
 const { createLocation, updateLocation, DeleteLocation } = require('../controller/adminController')
 
-
+//CREATE LOCATION
 router.post('/location', createLocation)
 
+//GET LOCATION
+router.get('/findLocation', findLocation)
+
+//UPDATE LOCATION
 router.put('/:locationId/updatelocation', updateLocation)
 
+//DELETE LOCATION
 router.delete('/:locationId/deletelocation', DeleteLocation)
 
-
-
-router.get('/findLocation',locationController.findLocation)
 
 module.exports = router
